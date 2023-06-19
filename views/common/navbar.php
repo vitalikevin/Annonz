595700d773f26dbc999458ae8731767a70972ff0
Kévin Vitali <kevin.vitali140@gmail.com>
2023-06-19 21:20:32 +0200

    général

--- views/common/navbar.php
+++ views/common/navbar.php
@@ -1,6 +1,6 @@
 <!-- Header -->
 <header id="header">
-    <a href="<?=URL?>index" class="title">Hyperspace</a>
+    <a href="<?=URL?>index" class="title">Anonzz</a>
     <nav>
         <ul>
             <!-- Avec la première version : en repartant de category_form pour aller vers la page d'accueil par exemple,
@@ -8,7 +8,7 @@
             L'ajout de "URL" ici permet de régler ce problème-->
             <li><a href="<?= URL ?>index" class="<?= str_contains(FULL_URL, "index") || str_contains(FULL_URL, "home") ? "active" : "" ?>">Accueil</a></li>
             <li><a href="<?= URL ?>categories" class="<?= str_contains(FULL_URL, "categories") ? "active" : "" ?>">Catégories</a></li>
-            <li><a href="<?= URL ?>ads"  class="<?= str_contains(FULL_URL, "ads") ? "active" : "" ?>">Annonces</a></li>
+            <li><a href="<?= URL ?>ads"  class="<?= str_contains(FULL_URL, "ads") ? "active" : "" ?>">Toutes les annonces</a></li>
             <li><a href="<?= URL ?>ads/high-tech" class="<?= str_contains(FULL_URL, "ads/high-tech") ? "active"  : "" ?>">High-Tech</a></li>
 				<li><a href="<?= URL ?>ads/mode" class="<?= str_contains(FULL_URL, "ads/mode") ? "active"  : "" ?>">Mode</a></li>
 				<li><a href="<?= URL ?>ads/vehicules" class="<?= str_contains(FULL_URL, "ads/vehicules") ? "active"  : "" ?>">Véhicules</a></li>
--- views/index.php
+++ views/index.php
@@ -4,9 +4,11 @@ if (isLogin()) {
 	echo "Bonjour " . $_SESSION['username'] . " ! ";
 } ?>
 
-<?= var_dump($_SESSION); ?>
 
+<?php require_once "views/common/header.php"; ?>
+<body class="is-preload">
 
+<?php require_once "views/common/navbar.php"; ?>
 
 <!DOCTYPE HTML>
 <!--
@@ -17,36 +19,19 @@ if (isLogin()) {
 <html>
 
 <head>
-	<title>Hyperspace by HTML5 UP</title>
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-	<link rel="stylesheet" href="public/assets/css/style.css" />
 	<link rel="stylesheet" href="public/assets/css/moncss.css" />
-	<noscript>
-		<link rel="stylesheet" href="public/assets/css/noscript.css" />
-	</noscript>
+
 </head>
 
-<body class="is-preload">
 
 
 	<!-- Header -->
 	<header id="header">
 		<nav>
 			<ul>
-				<!-- Boutons disponibles pour les utilisateurs connectés et les simples visiteurs -->
-				<li><a href="index"
-						class="<?= str_contains(FULL_URL, "index") || str_contains(FULL_URL, "home") ? "active" : "" ?>">Accueil</a>
-				</li>
-				<li><a href="categories"
-						class="<?= str_contains(FULL_URL, "categories") ? "active" : "" ?>">Catégories</a></li>
-				<li><a href="ads" class="<?= str_contains(FULL_URL, "ads") ? "active" : "" ?>">Annonces</a></li>
-				<li> <a href="ads/high-tech" class="<?= str_contains(FULL_URL, "ads/high-tech") ?  "active" : "" ?>">High-Tech</a></li>
-				<li><a href="ads/mode" class="<?= str_contains(FULL_URL, "ads/mode") ? "active"  : "" ?>">Mode</a></li>
-				<li><a href="ads/vehicules" class="<?= str_contains(FULL_URL, "ads/vehicules") ? "active"  : "" ?>">Véhicules</a></li>
-				<li><a href="ads/maison" class="<?= str_contains(FULL_URL, "ads/high-tech") ? "maison"  : "" ?>">Maison</a></li>
-				<li><a href="ads/divers" class="<?= str_contains(FULL_URL, "ads/divers") ? "active"  : "" ?>">Divers</a></li>
-				<li><a href="users" class="<?= str_contains(FULL_URL, "users") ? "active" : "" ?>">Users</a></li>
+
 
 				<!-- Boutons disponibles uniquement pour les utilisateurs connectés-->
 
@@ -67,7 +52,6 @@ if (isLogin()) {
 	</header>
 
 
-	<h1>Mes petites annonces</h1>
 	</div>
 	<div id="bar">
 		<!-- formulaire de recherche par mot clé -->
@@ -80,60 +64,15 @@ if (isLogin()) {
 			<input type="submit" value="Rechercher" name="rechercher" />
 		</form>
 	</div>
-	<div id="main">
-		<div id="column_left">
-			<h2>Catégories</h2>
-			<ul>
-				<li><a> Véhicule </a></li>
-				<li><a> Immobilier </a></li>
-				<li><a> Emploi </a></li>
-				<li><a> Rencontre </a></li>
-				<li><a> Mode </a></li>
-				<li><a> Objets </a></li>
-				<li><a> Animaux </a></li>
-			</ul>
-
-		</div>
-		<div class='row'>
-			<div class='jumbotron bg-light m-2 p-2'>
-				<h1 class='display-4'>Test titre h1 </h1>
-				<p class='lead'>Test texte</p>
-				<hr class='my-4'>
-				<p>Cliquer sur le bouton ci-dessous pour obtenir une liste des catégories</p>
-				<p class='lead'>
-					<a class='btn btn-primary btn-lg' href='categories.php' role='button'>Catégories</a>
-				</p>
-				<p>Cliquer sur le bouton ci-dessous pour obtenir une liste des annonces</p>
-				<p class='lead'>
-					<a class='btn btn-primary btn-lg' href='ads.php' role='button'>Annonces</a>
-				</p>
-				<p>Cliquer sur le bouton ci-dessous pour obtenir une liste des utilisateurs</p>
-				<p class='lead'>
-					<a class='btn btn-primary btn-lg' href='users.php' role='button'>Utilisateurs</a>
-				</p>
-			</div>
-
-		</div>
+	
+		
 
 
 
-		<p class='lead'>
-			<a class='btn btn-primary btn-lg' href='user_form.php' role='button'>Créer un compte</a>
-		</p>
+	<?php require_once "views/common/footer.php"; ?>
 
-		<p class='lead'>
-			<a class='btn btn-primary btn-lg' href='formulaire_connexion.php' role='button'>Connexion</a>
-		</p>
 
-		<!-- Footer -->
-		<footer id="footer" class="wrapper style1-alt">
-			<div class="inner">
-				<ul class="menu">
-					<li>&copy; Untitled. All rights reserved.</li>
-					<li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
-				</ul>
-			</div>
-		</footer>
+		
 </body>
 
 </html>
